5df884554bd5218f073d39c3e4f5fd8f045174a3
Agustin Catalan <cainagustincatalan@gmail.com>
2022-06-30 01:24:14 -0300

    login ok

--- root/componentes/login/login.php
+++ root/componentes/login/login.php
@@ -1,19 +1,43 @@
-<div class="container-fluid m-0 mb-5 p-0">
-  <div class="row justify-content-md-center m-0 p-0">
-    <div class="container-fluid col-md-4">
-      
-        <form>
-            <div class="mb-3">
-                <label for="user" class="form-label">Usuario</label>
-                <input type="text" class="form-control" id="user">
-            </div>
-            <div class="mb-3">
-                <label for="pass" class="form-label">Contraseña</label>
-                <input type="password" class="form-control" id="pass">
-            </div>
-            <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
-        </form>
+<?php
 
+$ErrorEnLogin = false;
+
+if (isset($_POST['login'])) {
+
+  if ($_POST['user'] == 'admin' && $_POST['pass'] == 'admin') {
+    
+    $_SESSION['user'] = 'admin';
+    header("Location: /index.php");
+
+  } else {
+    $ErrorEnLogin = true;
+  }
+}
+
+?>
+<div class="container-fluid m-0 mb-5 p-0">
+    <div class="row justify-content-md-center m-0 p-0">
+      <div class="container-fluid col-md-4">
+
+        <?php if ($ErrorEnLogin) { ?>
+          <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> usuario o contraseña incorrecta.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+        <?php } ?>
+        
+          <form method="POST" name="form_login">
+              <div class="mb-3">
+                  <label for="user" class="form-label">Usuario</label>
+                  <input type="text" class="form-control" id="user" name="user">
+              </div>
+              <div class="mb-3">
+                  <label for="pass" class="form-label">Contraseña</label>
+                  <input type="password" class="form-control" id="pass" name="pass">
+              </div>
+              <button type="submit" name="login" class="btn btn-primary">Iniciar Sesión</button>
+          </form>
+  
+      </div>
     </div>
-  </div>
 </div>
--- root/index.php
+++ root/index.php
@@ -8,25 +8,54 @@
     <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </head>
+
+<?php
+
+session_start();
+
+$SesionActiva = false;
+$UserActivo = null;
+
+if (isset($_SESSION['user'])) {
+    $SesionActiva = true;
+    $UserActivo = $_SESSION['user'];
+}
+
+?>
+
 <body>
     
     <header>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Disney Films</a>
+                <?php if ($SesionActiva) { ?>
+                    <div class="d-flex">
+                        <div class="container-fluid">
+                            <span class="navbar-text text-center">
+                                <?=$UserActivo?>
+                            </span>
+                            <a href="/servicios/cerrar_sesion.php" class="btn btn-outline-danger" role="button">Cerrar Sesión</a>
+                        </div>
+                    </div>
+                <?php } ?>
             </div>
         </nav>
     </header>
 
     <div class="container-fluid p-0 pt-2 mt-5">
 
+        <div class="container-fluid p-0 mt-5">
+            <?php 
+                if (!$SesionActiva) {
+                    include "./componentes/login/login.php";
+                }
+            ?>
+        </div>
+
         <main>
             <div class="container-fluid p-0 mt-5">
-
-                <?php
-                    include "./componentes/login/login.php"
-                ?>
-
+                main
             </div>
         </main>
 

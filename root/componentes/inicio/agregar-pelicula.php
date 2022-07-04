29ede445b9516ba07c22324a4fca94f04de5b856
Agustin Catalan <cainagustincatalan@gmail.com>
2022-07-04 19:54:02 -0300

    boton agregar pelicula solo cuando hay una sesion

--- root/componentes/inicio/agregar-pelicula.php
+++ root/componentes/inicio/agregar-pelicula.php
@@ -20,47 +20,51 @@ if (isset($_POST['boton_eliminar'])) {
 
 ?>
 
-<div class="container-fluid p-0 m-0">
-    <div class="d-flex justify-content-center">
-        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModalAgregarPelicula">Cargar Pelicula</button>
-    </div>
-</div>
+<?php if ($SesionActiva) { ?>
 
-<div class="modal fade" id="exampleModalAgregarPelicula" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Nueva Pelicula</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="container-fluid p-0 m-0">
+        <div class="d-flex justify-content-center">
+            <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModalAgregarPelicula">Cargar Pelicula</button>
         </div>
-        <form>
-            <div class="modal-body">
-                <div class="mb-3">
-                    <label for="titulo" class="col-form-label">Titulo</label>
-                    <input type="text" class="form-control" id="titulo">
-                </div>
-                <div class="mb-3">
-                    <label for="genero" class="col-form-label">Genero</label>
-                    <input type="text" class="form-control" id="genero">
-                </div>
-                <div class="mb-3">
-                    <label for="año" class="col-form-label">Año</label>
-                    <input type="text" class="form-control" id="año">
-                </div>
-                <div class="mb-3">
-                    <label for="descripcion" class="col-form-label">Descripción</label>
-                    <textarea class="form-control" id="descripcion"></textarea>
-                </div>
-                <div class="mb-3">
-                    <label for="urlimagen" class="col-form-label">URL de Imagen</label>
-                    <textarea class="form-control" id="urlimagen"></textarea>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                <button type="button" class="btn btn-primary">Confirmar</button>
-            </div>
-        </form>
     </div>
-  </div>
-</div>
+
+    <div class="modal fade" id="exampleModalAgregarPelicula" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Nueva Pelicula</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="titulo" class="col-form-label">Titulo</label>
+                            <input type="text" class="form-control" id="titulo">
+                        </div>
+                        <div class="mb-3">
+                            <label for="genero" class="col-form-label">Genero</label>
+                            <input type="text" class="form-control" id="genero">
+                        </div>
+                        <div class="mb-3">
+                            <label for="año" class="col-form-label">Año</label>
+                            <input type="text" class="form-control" id="año">
+                        </div>
+                        <div class="mb-3">
+                            <label for="descripcion" class="col-form-label">Descripción</label>
+                            <textarea class="form-control" id="descripcion"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="urlimagen" class="col-form-label">URL de Imagen</label>
+                            <textarea class="form-control" id="urlimagen"></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-primary">Confirmar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+<?php } ?>
